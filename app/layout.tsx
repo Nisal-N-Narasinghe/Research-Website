252e4e71b28708e3785c861ad834b8323a6fe4c5
IT21259302-Narasinghe <it21259302@my.sliit.lk>
2025-05-24 23:48:04 +0530

    change meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,14 +31,28 @@ export const metadata: Metadata = {
     siteName: "24-25J-216-Research",
     images: [
       {
-        url: "/images/logo_1.svg",
+        url: "/images/system_overview_diagram.png",
         width: 800,
         height: 600,
-        alt: "24-25J-216 Research Logo",
+        alt: "24-25J-216 Research System Overview Diagram",
       },
     ],
     locale: "en-US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@24-25J-216",
+    title: "24-25J-216-Research",
+    description: "LLM BASED MULTI-AGENT PLATFORM FOR WORKPLACE TASK AUTOMATION",
+    images: [
+      {
+        url: "/images/system_overview_diagram.png",
+        width: 800,
+        height: 600,
+        alt: "24-25J-216 Research System Overview Diagram",
+      },
+    ],
   },
 };
 
